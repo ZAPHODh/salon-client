84cf73e35b2635f5fda95491d3fe61432bac6ea3
zaphodh <luispaulo.ni@gmail.com>
2025-08-06 08:41:53 -0300

    fixed error in renderimgel

--- src/components/OGImgEl.tsx
+++ src/components/OGImgEl.tsx
@@ -13,7 +13,7 @@ export const RenderIMGEl = ({
         <div tw="flex relative flex-col p-12 w-full h-full rounded bg-gray-900 text-white items-center">
             <div tw="flex  items-center my-5">
                 <img src={logo} alt="ChadNext Logo" tw="h-10 mr-2" />
-                <div tw="text-xl font-bold tracking-tight text-white">ChadNext</div>
+                <div tw="text-xl font-bold tracking-tight text-white">PL</div>
                 <div
                     style={{
                         marginLeft: 10,
@@ -22,7 +22,7 @@ export const RenderIMGEl = ({
                     {locale ? "/" + locale : ""}
                 </div>
             </div>
-            <img src={image} alt="ChadNext Logo" tw=" rounded-lg" />
+            <img src={image} alt="PL Logo" tw=" rounded-lg" />
         </div>
     );
 };
