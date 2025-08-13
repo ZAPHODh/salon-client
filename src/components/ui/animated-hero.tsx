fda26d254f593937d3273875e41b258a34c3be98
zaphodh <luispaulo.ni@gmail.com>
2025-08-13 12:44:31 -0300

    removed contact button

--- src/components/ui/animated-hero.tsx
+++ src/components/ui/animated-hero.tsx
@@ -67,9 +67,6 @@ function Hero() {
             </p>
           </div>
           <div className="flex flex-row gap-3">
-            <Button size="lg" className="gap-4" variant="outline">
-              {t('buttons.contact')} <PhoneCall className="w-4 h-4" />
-            </Button>
             <Button size="lg" className="gap-4" onClick={() => push("auth/signup")}>
               {t('buttons.trial')} <MoveRight className="w-4 h-4" />
             </Button>
