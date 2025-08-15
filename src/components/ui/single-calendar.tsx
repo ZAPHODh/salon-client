9019507b4e69ba687a0f72e90c45424d839ab7ad
zaphodh <luispaulo.ni@gmail.com>
2025-08-15 10:50:47 -0300

    adujest single calendar

--- src/components/ui/single-calendar.tsx
+++ src/components/ui/single-calendar.tsx
@@ -4,22 +4,22 @@ import * as React from "react";
 import { DayPicker } from "react-day-picker";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
 
-import type { DayPickerSingleProps } from "react-day-picker";
+import type { PropsSingle } from "react-day-picker";
 
-function SingleCalendar({ className, classNames, showOutsideDays = true, selected, ...props }: DayPickerSingleProps) {
+function SingleCalendar({ selected, ...props }: PropsSingle) {
     const [currentMonth, setCurrentMonth] = React.useState<Date | undefined>(selected instanceof Date ? selected : undefined);
 
     return (
         <DayPicker
             selected={selected}
-            showOutsideDays={showOutsideDays}
+            // showOutsideDays={showOutsideDays}
             month={currentMonth}
             onMonthChange={setCurrentMonth}
-            className={cn("p-3", className)}
+            className={cn("p-3")}
             classNames={{
                 months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
                 month: "space-y-4",
@@ -46,11 +46,11 @@ function SingleCalendar({ className, classNames, showOutsideDays = true, selecte
                 day_disabled: "text-muted-foreground opacity-50",
                 day_range_middle: "aria-selected:bg-accent aria-selected:text-accent-foreground",
                 day_hidden: "invisible",
-                ...classNames,
+                // ...classNames,
             }}
             components={{
-                IconLeft: ({ className, ...props }) => <ChevronLeft className={cn("h-4 w-4", className)} {...props} />,
-                IconRight: ({ className, ...props }) => <ChevronRight className={cn("h-4 w-4", className)} {...props} />,
+                PreviousMonthButton: ({ ...props }) => <Button {...props}><ChevronLeft className={cn("h-4 w-4")} /></Button>,
+                NextMonthButton: ({ ...props }) => <Button {...props}><ChevronRight className={cn("h-4 w-4")} /></Button>,
             }}
             {...props}
         />
